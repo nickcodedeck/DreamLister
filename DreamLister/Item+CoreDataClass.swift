061d8746ed0f8ba54fb851cdc7b5ef7ce3f6560d
Nick Nguyen <nick.codedeck@gmail.com>
2017-05-05 23:49:15 -0700

    first commit

---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Nghia Nguyen on 5/2/17.
+//  Copyright © 2017 Slicknick. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Nghia Nguyen on 5/2/17.
+//  Copyright © 2017 Slicknick. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
